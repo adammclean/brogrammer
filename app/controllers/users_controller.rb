a3da5dea711dc25b94fff1002ae5bf1b726ef4dd
Adam McLean <adam.mclean1@gmail.com>
2015-03-19 14:48:58 -0700

    add user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       redirect_to root_url, :notice => "You're all signed up!"
     else
       render "new"
     end
   end
-end
 
+
+private
+  def user_params
+    params.require(:user).permit(:email, :password_hash, :password_salt)
+  end
+end
